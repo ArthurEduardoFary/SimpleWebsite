f484c84b6c60c0c3be20bb42a54a23a83a7ceda4
ArthurEduardoFary <arthurfary22@gmail.com>
2023-02-09 14:36:22 -0300

    First Commit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Nubas</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/patchnotes">Patchnotes</a>
+                <a href="https://github.com/ArthurEduardoFary/nubas" target={"_blank"}>Github</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
