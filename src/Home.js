e8bd58afb10ab7786c8ab6a9735444e5b2169c87
ArthurEduardoFary <arthurfary22@gmail.com>
2023-02-17 00:52:58 -0300

    added multiple language support and some more stuff in the page

--- src/Home.js
+++ src/Home.js
@@ -1,51 +1,86 @@
 import Carousel from 'react-bootstrap/Carousel';
-import firstimage from './img/one.png'
-import secondimage from './img/two.png'
+import firstimage from './img/one.jpg'
+import secondimage from './img/two.jpg'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+import { useTranslation } from 'react-i18next';
 
 const Home = () => {
+    
+    const { t, i18n } = useTranslation();
 
     return (  
     <div className="home">
         <div className='w-100'>
+            
         <Carousel>
             <Carousel.Item interval={5000}>
                 <img
                     style={{ objectFit: 'cover',  filter: 'contrast(80%) brightness(40%)' }}
-                    height={window.innerHeight}
+                    height={window.innerHeight/1.2}
                     className="d-block w-100"
                     src={firstimage}
                     alt="Image One"
                 />
                 <Carousel.Caption style={{top: '40%'}}>
-                    <h3>O primeiro</h3>
-                    <h1>Banco virtual do Minecraft</h1>
+                    <h3>{t('hello')}</h3>
+                    <h1>{t('iam')}</h1>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={5000}>
-                <img
-                    style={{ objectFit: 'cover', filter: 'contrast(80%) brightness(40%)'}}
-                    height={window.innerHeight}
-                    className="d-block w-100"
-                    src={secondimage}
+                <img style={{ objectFit: 'cover', filter: 'contrast(80%) brightness(40%)'}}
+                    height={window.innerHeight/1.2}
+                    className="d-block w-100"                  
                     alt="Image Two"
-                    
-                    
-                />
-                <Carousel.Caption>
-                    <h3>Label for second slide</h3>
-                    <p>Sample Text for Image Two</p>
+                    src={secondimage}/>
+                <Carousel.Caption style={{top: '40%'}}>
+                    <h3>{t('iam2')}</h3>
+                    <h1>Full-Stack</h1>
+                    <h3>{t('dev')}</h3>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
+
+        <Container id='homecontainer'>
+
+            <Row>
+                <Col>
+                    <h1 className='text-center mt-5 pt-5' style={{fontSize: '50pt'}}>{t('aboutme')}</h1>
+                </Col>
+            </Row>
+
+            <Row className='mt-5'>
+                <Col xs={12} md={4} >
+                    <div style={{backgroundColor: 'red'}} className='text-md-end text-center mb-md-0 mb-5'> aqui vai uma foto mto massa</div>
+                </Col>
+                <Col xs={12} md={8}>
+                    <div className='text-md-start text-center'> 
+                    <p>My name is Arthur, I am a junior Full-Stack developer 
+                        with a on-going bachelors in Informational Systems. 
+                    </p>
+                    <p>
+                        I would describe myself as a coding enthusiast who really enjoys developping stuff. 💻
+                    </p>
+                    </div>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                <h1 className='text-center mt-5 pt-5' style={{fontSize: '50pt'}}>{t('intrestedin')}</h1>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col></Col>
+            </Row>
+        </Container>
+
         </div>
-
-        {/* test */}
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nostrum mollitia, dolorum aperiam veritatis autem aut quia voluptatum facilis eaque totam consequuntur similique accusamus veniam, quisquam explicabo velit corporis aspernatur.</p>
-
-
     </div>
     );
 }
- 
 export default Home;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,26 @@
+
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import { useState, useEffect } from "react"
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+import { useTranslation } from 'react-i18next';
+
 
 function MainNavbar() {
+    
+
+    const { t, i18n } = useTranslation();
+
+    const changeTranslationHandler = lng => {
+      i18n.changeLanguage(lng)
+    }
+
     //CREATE CLASS with variable 'navbar' set as default to false
     const [navbar, setNavbar] = useState(false)
 
@@ -30,10 +46,8 @@ function MainNavbar() {
     {/* backuphere */}
       <Navbar collapseOnSelect expand='sm' fixed='top' className={(navbar ? "navbar active" : "navbar navbar-dark")}>
           <Container>
-            
-            
 
-            <Navbar.Brand href="#" ><h1  className='ms-5'>Nubas</h1> </Navbar.Brand>
+            <Navbar.Brand href="#" ><h1  className='ms-5 pt-2'>Arthur</h1> </Navbar.Brand>
             
             <Navbar.Toggle className='humburger me-5 custom-toggler' aria-controls="navbarHamburger" data-bs-toggle="collapse" data-bs-target="#navbarHamburger" />
             <Navbar.Offcanvas
@@ -49,26 +63,46 @@ function MainNavbar() {
               <Offcanvas.Body className='me-5 justify-content-end'>
             
             <Nav class="navbar-nav" >
-                <Nav.Link href='#' className='pe-3'>
-                    Home
-                </Nav.Link>
-                <Nav.Link href='#' className='pe-3'>
-                    Patchnotes
-                </Nav.Link>
                 <Nav.Link href='https://github.com/ArthurEduardoFary/SimpleWebsite' target='__blank' className='pe-3'>
                     Git-hub
                 </Nav.Link>
-                <Nav.Link href='https://www.linkedin.com/in/arthur-eduardo-fary-199346213/' target='__blank' className='pe-3 me-5'>
+                <Nav.Link href='https://www.linkedin.com/in/arthur-eduardo-fary-199346213/' target='__blank' className='pe-3'>
                     Linked-In
                 </Nav.Link>
+                <NavDropdown title={t('contactme')} id='nav-dropdown' className='pe-3'> 
+                  <NavDropdown.Item href={'http://wa.me/5547996762167?text=' + t('whatsapp')} target='__blank'>
+                    Whatsapp
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href='mailto:arthur.e.fary@gmail.com'>
+                    E-mail
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href='https://www.instagram.com/arthurfary/' target='__blank'>
+                    Instagram
+                  </NavDropdown.Item>
+              
+                </NavDropdown>
+                
+                <Col >
+                  <NavDropdown title={t('lang')} id='nav-dropdown' className='pe-5 '> 
+                          <NavDropdown.Item onClick={() => {changeTranslationHandler('en')}}>
+                            English
+                          </NavDropdown.Item>
+                          <NavDropdown.Item onClick={() => {changeTranslationHandler('pt')}}>
+                            Português
+                          </NavDropdown.Item>
+                  </NavDropdown>
+                </Col>
             </Nav>
+            
 
             </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
+          
       </Navbar>
     </>
   );
 }
 
-export default MainNavbar;
+
+export default MainNavbar
